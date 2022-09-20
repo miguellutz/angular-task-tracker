c7709c7d4f11e1b75e9e780d8d66527bc0652f99
Miguel Lutz <miguellutz31@gmail.com>
2022-09-20 17:00:43 +0200

    List tasks from mock-tasks. Interface for individual task. Import fontawesome icons. Implement fa-times to later delete task.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; // used to interact with the DOM
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'; // root component; When importing components through CLI will do this automatically
 import { HeaderComponent } from './components/header/header.component';
-import { ButtonComponent } from './components/button/button.component'; // root component; When importing components through CLI will do this automatically
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [], // global services
   bootstrap: [AppComponent]
