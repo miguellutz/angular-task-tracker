ce343fd3172ab5e137bc52939d1dceee49f0b4f5
Miguel Lutz <miguellutz31@gmail.com>
2022-09-21 17:10:38 +0200

    Add task form, onSubmit event emitter on form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; // used to interact with the DOM
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component'; // root component; When importing components through CLI will do this automatically
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 @NgModule({
   declarations: [ // declarations for components
@@ -16,12 +17,14 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    AddTaskComponent
   ],
   imports: [ // imports for modules
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [], // global services
   bootstrap: [AppComponent]
